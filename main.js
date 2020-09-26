08cefc6cf2cdc2e1f936bbb19032cece80fbddcb
tetsuya.kojima <tetsuya.kojima@fox-hound.tech>
2020-09-27 08:14:09 +0900

    データの読み込み機能実装

--- main.js
+++ main.js
@@ -16,9 +16,24 @@ $('#change').on('click', () => {
   }
   let confirm = window.confirm('登録します。よろしいですか?');
   if(confirm) {
-    localStorage['name'] = name;
-    localStorage['email'] = email;
-    localStorage['phone'] = phone;
-    // alert('名前:' + name + '\n' + 'Eメール:' + email + '\n' + '電話番号:' + phone + '\n' + 'で登録しました！');
+    const STORAGE_KEY_NAME = 'name';
+    const STORAGE_KEY_EMAIL = 'email';
+    const STORAGE_KEY_PHONE = 'phone';
+    localStorage.setItem(STORAGE_KEY_NAME, name);
+    localStorage.setItem(STORAGE_KEY_EMAIL, email);
+    localStorage.setItem(STORAGE_KEY_PHONE, phone);
+    alert('名前:' + name + '\n' + 'Eメール:' + email + '\n' + '電話番号:' + phone + '\n' + 'で登録しました！');
+  }
+});
+
+$('#fetch').on('click', () => {
+  let confirm = window.confirm('登録済みのデータを読み込みます。よろしいですか？')
+  if(confirm) {
+    let name = localStorage['name'];
+    let email = localStorage['email'];
+    let phone = localStorage['phone'];
+    $("#name").val(name);
+    $("#email").val(email);
+    $("#phone").val(phone);
   }
 });
